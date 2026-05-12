b9d9e7c8758ef2dafac5214c88f30f740a5451c4
automatizacionesfernando <automatizacionesfernando@gmail.com>
2026-05-12 13:09:29 +0200

    Fix: anadir barra final en base para que BASE_URL concatene bien

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://automatizacionesfernando.github.io',
-  base: '/optifatima-web',
+  base: '/optifatima-web/',
 });
